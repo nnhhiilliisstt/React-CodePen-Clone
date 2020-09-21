6710ae0315bd2ca9e3ff665e6c35d13b3ba03106
nnhhiilliisstt <kumar.aditya221b@email.com>
2020-09-21 08:38:53 +0530

    code review done and indentation fixes done :rocket:

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import Editor from './Editor'
-import useLocalStorage from '../hooks/useLocalStorage'
+import Editor from './Editor';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 function App() {
-  const [html, setHtml] = useLocalStorage('html', '')
-  const [css, setCss] = useLocalStorage('css', '')
-  const [js, setJs] = useLocalStorage('js', '')
-  const [srcDoc, setSrcDoc] = useState('')
+  const [html, setHtml] = useLocalStorage('html', '');
+  const [css, setCss] = useLocalStorage('css', '');
+  const [js, setJs] = useLocalStorage('js', '');
+  const [srcDoc, setSrcDoc] = useState('');
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -16,46 +16,46 @@ function App() {
           <style>${css}</style>
           <script>${js}</script>
         </html>
-      `)
-    }, 250)
+      `);
+    }, 250);
 
-    return () => clearTimeout(timeout)
-  }, [html, css, js])
+    return () => clearTimeout(timeout);
+  }, [html, css, js]);
 
   return (
     <>
-      <div className="pane top-pane">
+      <div className='pane top-pane'>
         <Editor
-          language="xml"
-          displayName="HTML"
+          language='xml'
+          displayName='HTML'
           value={html}
           onChange={setHtml}
         />
         <Editor
-          language="css"
-          displayName="CSS"
+          language='css'
+          displayName='CSS'
           value={css}
           onChange={setCss}
         />
         <Editor
-          language="javascript"
-          displayName="JS"
+          language='javascript'
+          displayName='JS'
           value={js}
           onChange={setJs}
         />
       </div>
-      <div className="pane">
+      <div className='pane'>
         <iframe
           srcDoc={srcDoc}
-          title="output"
-          sandbox="allow-scripts"
-          frameBorder="0"
-          width="100%"
-          height="100%"
+          title='output'
+          sandbox='allow-scripts'
+          frameBorder='0'
+          width='100%'
+          height='100%'
         />
       </div>
     </>
-  )
+  );
 }
 
 export default App;
