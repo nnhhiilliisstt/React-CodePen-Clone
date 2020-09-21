0035e2f019b7292dea3e44888aa4b52646fa5c09
nnhhiilliisstt <kumar.aditya221b@email.com>
2020-09-21 08:15:00 +0530

    Updates

---
+++ precache-manifest.e75f143f83ab03c690d991269bec1005.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19650d9145b27bb285d9ba51f7963a1d",
+    "url": "/React-CodePen-Clone/index.html"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/React-CodePen-Clone/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "8a0e6a250eaca14df1e6",
+    "url": "/React-CodePen-Clone/static/css/main.8a79c027.chunk.css"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/React-CodePen-Clone/static/js/2.4254f81d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-CodePen-Clone/static/js/2.4254f81d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a0e6a250eaca14df1e6",
+    "url": "/React-CodePen-Clone/static/js/main.8db9b6e8.chunk.js"
+  },
+  {
+    "revision": "7e5e599d4fb68665ae2b",
+    "url": "/React-CodePen-Clone/static/js/runtime-main.815072db.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React-CodePen-Clone/precache-manifest.e75f143f83ab03c690d991269bec1005.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-CodePen-Clone/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
